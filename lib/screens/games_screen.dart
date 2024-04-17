5873ffd323c460825ed97a96ef21317edce4d81a
Victor78970513 <yer29.chok@gmail.com>
2024-04-16 20:41:01 -0400

    4

--- lib/screens/games_screen.dart
+++ lib/screens/games_screen.dart
@@ -16,6 +16,7 @@ class _GameScreenState extends State<GameScreen> {
   void traerJuegos() async {
     // PRIMER COMMIT PRUEBA
     // SEGUNDO COMMIT PRUEBA2
+    // 4
 
     final respuesta =
         await http.get(Uri.parse("https://www.freetogame.com/api/games"));
